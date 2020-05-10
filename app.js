074d03429b0dd34d0d7b44150b3752157c272873
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2020-05-10 16:37:13 -0400

    initial constructor and controllers completed in app.js/ next coming states

---
+++ app.js
@@ -0,0 +1,35 @@
+//Stoage Controller
+
+//Item Controller
+
+/*item controller ify statement running immediately */
+const ItemCtrl = (function(){
+    //item constructor
+    const Item = function(id, name, calories){
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+    //data structure/state
+    const data = {
+        items: [
+            {id: 0, name: 'Steak Dinner', calories: 1200},
+            {id: 0, name: 'Eggs', calories: 400},
+            {id: 0, name: 'Omelette', calories: 800}
+
+        ], 
+        currentItem: null,
+        totalCalories: 0
+    }
+})();
+
+//UI Controller
+const UICtrl = (function(){
+
+})();
+
+
+//Application Controller
+const App = (function(ItemCtrl, UICtrl){
+
+})(ItemCtrl, UICtrl);
